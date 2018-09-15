51e22c90370cc87e96c2f78249e1b198f6275481
Diwank Tomer <diwank.singh@gmail.com>
2018-09-15 16:54:19 -0700

    Publish compiled version
    Signed-off-by: Diwank Tomer <diwank.singh@gmail.com>

--- babel.config.js
+++ babel.config.js
@@ -20,5 +20,24 @@ module.exports = {
         ],
       ],
     },
+    es5: {
+      plugins: [['@babel/plugin-proposal-class-properties', { loose: false }]],
+      presets: [
+        [
+          '@invertase/react-native-syntax',
+          {
+            flow: 'strip',
+          },
+        ],
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: '6',
+            },
+          },
+        ],
+      ],
+    },
   },
 };
